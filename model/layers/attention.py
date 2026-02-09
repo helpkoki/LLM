4242f61c5ecc7af3ae16b85fd13501b23a952ace
mrk luffy <helpself87@gmail.com>
2026-02-09 22:00:03 +0200

    all to test dammm

---
+++ model/layers/attention.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+class SimpleSelfAttention:
+     def __init__(self , embed_dim):
+         self.embed_dim = embed_dim
+
+         self.W_q = np.random.rand(embed_dim, embed_dim) *0.02
+         self.W_k = np.random.rand(embed_dim, embed_dim) *0.02
+         self.W_v = np.random.rand(embed_dim, embed_dim) *0.02
+
+     def forward(self, x):
+          
+          batch_size, seq_len, embed_dim = x.shape
+
+          Q = np.dot(x, self.W_q)
+          K = np.dot(x, self.W_k)
+          V = np.dot(x, self.W_v)
+
+          scores = np.matmul(Q, K.transpose(0, 2, 1)) / np.sqrt(self.embed_dim)
+          attention_weights = self._softmax(scores)
+          output = np.matmul(attention_weights, V)
+
+          return output
+     
+     def _softmax(self, x):
+            e_x = np.exp(x - np.max(x, axis=-1, keepdims=True))
+            return e_x / np.sum(e_x, axis=-1, keepdims=True)
--- model/layers/embeddings.py
+++ model/layers/embeddings.py
@@ -11,3 +11,24 @@ class SimpleEmbedding:
 
     def forward(self, input_indices):
         return self.embeddings[input_indices]
+    
+  
+
+class SimplePositionalEncoding:
+    def __init__(self, max_len, embed_dim):
+        self.max_len = max_len
+        self.embed_dim = embed_dim
+        self.positional_encoding = self._create_positional_encoding()
+
+    def _create_positional_encoding(self):
+        positional_encoding = np.zeros((self.max_len, self.embed_dim))
+        for pos in range(self.max_len):
+            for i in range(0, self.embed_dim, 2):
+                positional_encoding[pos, i] = np.sin(pos / (10000 ** (i / self.embed_dim)))
+                if i + 1 < self.embed_dim:
+                    positional_encoding[pos, i + 1] = np.cos(pos / (10000 ** ((i + 1) / self.embed_dim)))
+        return positional_encoding
+
+    def forward(self, input_indices):
+        batch_size, seq_len = input_indices.shape
+        return self.positional_encoding[:seq_len]
